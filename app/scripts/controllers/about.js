c12b3c60c1e585987fc7b881f416c8bf37c2890e
Hafþór Snær Þórsson <hafthort12@ru.is>
2016-03-12 03:38:58 +0000

    added yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name haffiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the haffiApp
+ */
+angular.module('haffiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
