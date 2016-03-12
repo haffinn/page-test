e2464ce84cb029a855d6631a1d70f8e7c1d85c05
Hafþór Snær Þórsson <hafthort12@ru.is>
2016-03-12 05:20:37 +0000

    copy CNAME to dist in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -433,7 +433,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'CNAME'
           ]
         }, {
           expand: true,
