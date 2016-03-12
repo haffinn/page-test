89c24938d51bb2d9d26560756512224084140363
Hafþór Snær Þórsson <hafthort12@ru.is>
2016-03-12 05:08:31 +0000

    renamed task in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
     yeoman: appConfig,
 
     // publish to github pages
-    buildcontrol: {
+    publish: {
       options: {
         dir: 'dist',
         commit: true,
